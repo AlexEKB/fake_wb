623b212cec022ed2f70f5057f934e72768495b4e
alexekb <pu4kom@mail.ru>
2019-03-21 11:04:27 +0500

    gem twitter-bootstrap

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'devise'
+gem 'jquery-rails'
+gem 'twitter-bootstrap-rails'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    end
+    devise_parameter_sanitizer.permit :sign_up, keys: [:first_name, :last_name, :phone, :birthday]
+  end
 end
